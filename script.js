2cff69239c7f4bd87f9403389274a07a9ef6d76c
Fatima Montealegre <120394684+chokwit@users.noreply.github.com>
2023-11-16 05:18:08 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+<style>
+  
+.title {
+  font-family:'Times New Roman';
+  font-size: 30px;
+  font-weight: bold;
+  font-style: italic;
+  text-align:center;
+  line-height: 24px;
+  color:rgb(44, 110, 88);
+}
+.Prac {
+  font-family: 'Lucida Sans';
+  color:blueviolet;
+}
+
+.OrgChem-button {
+  cursor: pointer;
+}
+
+.AnaChem-button {
+  cursor: pointer;
+}
+</style>
+
+<p class="title"> 
+  Chemical Technician Free Reviewer
+</p>
+
+<a href="https://www.facebook.com/permalink.php?story_fbid=pfbid02NRr3XVoiWY4DBszWoy7WdFn3Nqf152najwmB2jyfbZZNftnDVEzyv2q12a4L5mR5l&id=100095594888759"
+ target="_blank" >Table of Specifications (TOS)
+</a>  
+
+<a href="https://www.prc.gov.ph/requirements/chemical-technician"
+ target="_blank" >Qualifications and Requirements
+</a>
+
+<p class="Prac">
+ Free Practice Tests
+</p>
+
+<button class="OrgChem-button">
+  Organic Chemistry
+</button>
+
+<button class="AnaChem-button">
+  Analytical Chemistry
+</button>
